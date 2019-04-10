f63ce9fa223fd6a7ca9b35c1808267d6c3802384
靳炳淑 <jinbingshu_nju@qq.com>
2019-04-10 11:21:38 +0800

    schools fade样式

--- src/pages/home/Schools/index.js
+++ src/pages/home/Schools/index.js
@@ -12,25 +12,48 @@ function Icon(props) {
   );
 }
 
+const artUniversityTitle = Object.keys(artUniversity);
+const artUniversityTitle1 = artUniversityTitle.slice(0, 4);
+const artUniversityTitle2 = artUniversityTitle.slice(4, 8);
+const artUniversityTitle3 = artUniversityTitle.slice(8, 12);
+const artUniversityTitle4 = artUniversityTitle.slice(12, 16);
+
+const universityTitle = Object.keys(university);
+const universityTitle1 = universityTitle.slice(0, 2);
+const universityTitle2 = universityTitle.slice(2, 4);
+const universityTitle3 = universityTitle.slice(4, 6);
+const universityTitle4 = universityTitle.slice(6, 8);
+
+const museumTitle = Object.keys(museum);
+const museumTitle1 = museumTitle.slice(0, 1);
+const museumTitle2 = museumTitle.slice(1, 2);
+const museumTitle3 = museumTitle.slice(2, 3);
+const museumTitle4 = museumTitle.slice(3, 4);
+
 export default class Schools extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      group: 0
+    };
+
+    setInterval(() => {
+      const { group } = this.state;
+      this.setState({
+        group: group === 0 ? 1 : 0
+      });
+    }, 6000);
+  }
+
   render() {
-    const artUniversityTitle = Object.keys(artUniversity);
-    const artUniversityTitle1 = artUniversityTitle.slice(0, 4);
-    const artUniversityTitle2 = artUniversityTitle.slice(4, 8);
-    const artUniversityTitle3 = artUniversityTitle.slice(8, 12);
-    const artUniversityTitle4 = artUniversityTitle.slice(12, 16);
+    const { group } = this.state;
 
-    const universityTitle = Object.keys(university);
-    const universityTitle1 = universityTitle.slice(0, 2);
-    const universityTitle2 = universityTitle.slice(2, 4);
-    const universityTitle3 = universityTitle.slice(4, 6);
-    const universityTitle4 = universityTitle.slice(6, 8);
-
-    const museumTitle = Object.keys(museum);
-    const museumTitle1 = museumTitle.slice(0, 1);
-    const museumTitle2 = museumTitle.slice(1, 2);
-    const museumTitle3 = museumTitle.slice(2, 3);
-    const museumTitle4 = museumTitle.slice(3, 4);
+    const artg1 = group === 0 ? artUniversityTitle1 : artUniversityTitle3;
+    const artg2 = group === 0 ? artUniversityTitle2 : artUniversityTitle4;
+    const universityg1 = group === 0 ? universityTitle1 : universityTitle3;
+    const universityg2 = group === 0 ? universityTitle2 : universityTitle4;
+    const museumg1 = group === 0 ? museumTitle1 : museumTitle3;
+    const museumg2 = group === 0 ? museumTitle2 : museumTitle4;
 
     return (
       <div className="schools_body">
@@ -41,14 +64,14 @@ export default class Schools extends Component {
         <div className="main_container">
           <div className="art">
             <div className="oneline">
-              {artUniversityTitle1.map(title => {
+              {artg1.map(title => {
                 return (
                   <Icon title={title} url={artUniversity[title].imageUrl} />
                 );
               })}
             </div>
             <div className="oneline">
-              {artUniversityTitle2.map(title => {
+              {artg2.map(title => {
                 return (
                   <Icon title={title} url={artUniversity[title].imageUrl} />
                 );
@@ -58,12 +81,12 @@ export default class Schools extends Component {
           <div className="split" />
           <div className="university">
             <div className="oneline">
-              {universityTitle1.map(title => {
+              {universityg1.map(title => {
                 return <Icon title={title} url={university[title].imageUrl} />;
               })}
             </div>
             <div className="oneline">
-              {universityTitle2.map(title => {
+              {universityg2.map(title => {
                 return <Icon title={title} url={university[title].imageUrl} />;
               })}
             </div>
@@ -71,12 +94,12 @@ export default class Schools extends Component {
           <div className="split" />
           <div className="museum">
             <div className="oneline">
-              {museumTitle1.map(title => {
+              {museumg1.map(title => {
                 return <Icon title={title} url={museum[title].imageUrl} />;
               })}
             </div>
             <div className="oneline">
-              {museumTitle2.map(title => {
+              {museumg2.map(title => {
                 return <Icon title={title} url={museum[title].imageUrl} />;
               })}
             </div>
