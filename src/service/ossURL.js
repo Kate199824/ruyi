90c5afb82f2873209e6673b3b66e87998fba8dcf
靳炳淑 <jinbingshu_nju@qq.com>
2019-04-10 08:04:21 +0800

    修改url错误

--- src/service/ossURL.js
+++ src/service/ossURL.js
@@ -24,7 +24,7 @@ export const photos = [
   'http://ruyiart.oss-cn-beijing.aliyuncs.com/photos/p3.png',
   'http://ruyiart.oss-cn-beijing.aliyuncs.com/photos/p4.png',
   'http://ruyiart.oss-cn-beijing.aliyuncs.com/photos/p5.png',
-  'http://ruyiart.oss-cn-beijing.aliyuncs.com/photos/p5.png',
+  'http://ruyiart.oss-cn-beijing.aliyuncs.com/photos/p6.png',
   'http://ruyiart.oss-cn-beijing.aliyuncs.com/photos/p7.png',
   'http://ruyiart.oss-cn-beijing.aliyuncs.com/photos/p8.png'
 ];
@@ -38,3 +38,40 @@ export const bg_bar =
   'http://ruyiart.oss-cn-beijing.aliyuncs.com/background/bar.png';
 export const bg_photo =
   'http://ruyiart.oss-cn-beijing.aliyuncs.com/background/photoBg.png';
+
+export const university = [
+  'http://ruyiart.oss-cn-beijing.aliyuncs.com/schools/university/%E4%B8%9C%E5%8D%97%E5%A4%A7%E5%AD%A6.png',
+  'http://ruyiart.oss-cn-beijing.aliyuncs.com/schools/university/%E5%8D%97%E4%BA%AC%E5%A4%A7%E5%AD%A6.png',
+  'http://ruyiart.oss-cn-beijing.aliyuncs.com/schools/university/%E5%8D%97%E4%BA%AC%E5%B8%88%E8%8C%83%E5%A4%A7%E5%AD%A6.png',
+  'http://ruyiart.oss-cn-beijing.aliyuncs.com/schools/university/%E5%8D%97%E4%BA%AC%E6%9E%97%E4%B8%9A%E5%A4%A7%E5%AD%A6.png',
+  'http://ruyiart.oss-cn-beijing.aliyuncs.com/schools/university/%E5%8D%97%E4%BA%AC%E7%90%86%E5%B7%A5%E5%A4%A7%E5%AD%A6.png',
+  'http://ruyiart.oss-cn-beijing.aliyuncs.com/schools/university/%E5%8D%97%E4%BA%AC%E8%88%AA%E7%A9%BA%E8%88%AA%E5%A4%A9%E5%A4%A7%E5%AD%A6.png',
+  'http://ruyiart.oss-cn-beijing.aliyuncs.com/schools/university/%E5%8D%97%E4%BA%AC%E9%82%AE%E7%94%B5%E5%A4%A7%E5%AD%A6.png',
+  'http://ruyiart.oss-cn-beijing.aliyuncs.com/schools/university/%E6%B2%88%E9%98%B3%E5%B8%88%E8%8C%83%E5%A4%A7%E5%AD%A6.png'
+];
+
+export const museum = [
+  'http://ruyiart.oss-cn-beijing.aliyuncs.com/schools/museum/%E5%8D%97%E4%BA%AC%E5%8D%9A%E7%89%A9%E9%99%A2.png',
+  'http://ruyiart.oss-cn-beijing.aliyuncs.com/schools/museum/%E6%95%85%E5%AE%AB%E5%8D%9A%E7%89%A9%E9%99%A2.png',
+  'http://ruyiart.oss-cn-beijing.aliyuncs.com/schools/museum/%E6%9D%AD%E5%B7%9E%E5%8D%9A%E7%89%A9%E9%A6%86.png',
+  'http://ruyiart.oss-cn-beijing.aliyuncs.com/schools/museum/%E8%8B%8F%E5%B7%9E%E5%8D%9A%E7%89%A9%E9%A6%86.png'
+];
+
+export const artUniversity = [
+  'http://ruyiart.oss-cn-beijing.aliyuncs.com/schools/artUniversity/%E4%B8%8A%E6%B5%B7%E6%88%8F%E5%89%A7%E5%AD%A6%E9%99%A2.png',
+  'http://ruyiart.oss-cn-beijing.aliyuncs.com/schools/artUniversity/%E4%B8%AD%E5%9B%BD%E4%BC%A0%E5%AA%92%E5%A4%A7%E5%AD%A6.png',
+  'http://ruyiart.oss-cn-beijing.aliyuncs.com/schools/artUniversity/%E4%B8%AD%E5%9B%BD%E7%BE%8E%E9%99%A2.png',
+  'http://ruyiart.oss-cn-beijing.aliyuncs.com/schools/artUniversity/%E4%B8%AD%E5%A4%AE%E7%BE%8E%E9%99%A2.png',
+  'http://ruyiart.oss-cn-beijing.aliyuncs.com/schools/artUniversity/%E5%8C%97%E4%BA%AC%E6%9C%8D%E8%A3%85%E5%AD%A6%E9%99%A2.png',
+  'http://ruyiart.oss-cn-beijing.aliyuncs.com/schools/artUniversity/%E5%8C%97%E4%BA%AC%E7%94%B5%E5%BD%B1%E5%AD%A6%E9%99%A2.png',
+  'http://ruyiart.oss-cn-beijing.aliyuncs.com/schools/artUniversity/%E5%8D%97%E4%BA%AC%E8%89%BA%E6%9C%AF%E5%AD%A6%E9%99%A2.png',
+  'http://ruyiart.oss-cn-beijing.aliyuncs.com/schools/artUniversity/%E5%9B%9B%E5%B7%9D%E7%BE%8E%E9%99%A2.png',
+  'http://ruyiart.oss-cn-beijing.aliyuncs.com/schools/artUniversity/%E5%A4%A9%E6%B4%A5%E7%BE%8E%E9%99%A2.png',
+  'http://ruyiart.oss-cn-beijing.aliyuncs.com/schools/artUniversity/%E5%B1%B1%E4%B8%9C%E5%B7%A5%E8%89%BA%E7%BE%8E%E6%9C%AF%E5%AD%A6%E9%99%A2.png',
+  'http://ruyiart.oss-cn-beijing.aliyuncs.com/schools/artUniversity/%E5%B9%BF%E5%B7%9E%E7%BE%8E%E9%99%A2.png',
+  'http://ruyiart.oss-cn-beijing.aliyuncs.com/schools/artUniversity/%E6%B1%9F%E5%8D%97%E5%A4%A7%E5%AD%A6.png',
+  'http://ruyiart.oss-cn-beijing.aliyuncs.com/schools/artUniversity/%E6%B8%85%E5%8D%8E%E7%BE%8E%E9%99%A2.png',
+  'http://ruyiart.oss-cn-beijing.aliyuncs.com/schools/artUniversity/%E6%B9%96%E5%8C%97%E7%BE%8E%E9%99%A2.png',
+  'http://ruyiart.oss-cn-beijing.aliyuncs.com/schools/artUniversity/%E8%A5%BF%E5%AE%89%E7%BE%8E%E9%99%A2.png',
+  'http://ruyiart.oss-cn-beijing.aliyuncs.com/schools/artUniversity/%E9%B2%81%E8%BF%85%E7%BE%8E%E9%99%A2.png'
+];
